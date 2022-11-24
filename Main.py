d2547f5b694dc094b046a3d4ab8f28af6a729a55
Fireblade2534 <77405729+fireblade2534@users.noreply.github.com>
2022-11-24 14:00:39 +0000

    dafds

---
+++ Main.py
@@ -0,0 +1,15 @@
+class Board:
+    def __init__(self,Width,Height):
+        self.Width=Width
+        self.Height=Height
+        self.Board=[]
+        for X in range(Width):
+            self.Board.append(["" for Y in range(Height)])
+    
+    def DropPeice(self,Column):
+        for X in self.Board[Column]:
+            print()
+
+
+GameBoard=Board(5,5)
+        
