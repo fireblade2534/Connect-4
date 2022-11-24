9137d6694be68c54cc743c75e94710351f6a8dc2
Fireblade2534 <77405729+fireblade2534@users.noreply.github.com>
2022-11-24 16:28:30 +0000

    asdfads

--- Main.py
+++ Main.py
@@ -1,8 +1,13 @@
+
+def AllSame(List):
+    return len(list(set(List))) == 1
+
 class Board:
-    def __init__(self,Width,Height):
+    def __init__(self,Width,Height,WinNumber=4):
         self.Width=Width
         self.Height=Height
         self.Board=[]
+        self.WinNumber=WinNumber
         for X in range(Width):
             self.Board.append([" " for Y in range(Height)])
     
@@ -21,8 +26,70 @@ class Board:
         return False
 
 
-GameBoard=Board(5,5)
+    def CheckDiagonals(self):
+        CounterX=0
+        while CounterX <= self.Width - self.WinNumber or CounterX == 0:
+            
+            CounterY=0
+            while CounterY <= self.Height - self.WinNumber or CounterY == 0:
+                print(CounterX,CounterY)
+                
+                ListRD=[]
+                for A in range(0,self.WinNumber):
+                    #print(CounterX + A, CounterY + A,"R")
+                    ListRD.append(self.Board[CounterX + A][CounterY + A])
+               
+
+
+                ListLD=[]
+                for A in range(0,self.WinNumber):
+                    #print(CounterX + A, len(self.Board[CounterX]) - (CounterY + A) - 1,"L")
+                    ListLD.append(self.Board[CounterX + A][self.Height - (CounterY + A) - 1])
+
+
+                if AllSame(ListLD) and ListLD[0] != " ":
+                    return [True,ListLD[0]]
+                if AllSame(ListRD) and ListRD[0] != " ":
+                    return [True,ListRD[0]]
+                
+                print(ListLD,ListRD)
+                CounterY+=1
+            CounterX+=1
+        return [False]
+    def CheckRow(self):
+        CounterX=0
+        while CounterX <= self.Width - self.WinNumber or CounterX == 0:
+            for Y in range(self.Height):
+                List=[]
+                for A in range(self.WinNumber):
+                    List.append(self.Board[CounterX + A][Y])
+                #print(List)
+                if AllSame(List) and List[0] != " ":
+                    return [True,List[0]]
+            CounterX+=1
+        return [False]
+
+    def CheckCol(self):
+        CounterY=0
+        while CounterY <= self.Height - self.WinNumber or CounterY == 0:
+            for X in range(self.Width):
+                List=[]
+                for A in range(self.WinNumber):
+                    List.append(self.Board[X][CounterY + A])
+                #print(List)
+                if AllSame(List) and List[0] != " ":
+                    return [True,List[0]]
+            CounterY+=1
+        return [False]
+
+
+GameBoard=Board(7,6)
+print(GameBoard.Board)
 GameBoard.PrintBoard() 
+
+
 print("\n\n")
-GameBoard.DropPeice(0,"R")
-GameBoard.PrintBoard()
+GameBoard.PrintBoard() 
+print(GameBoard.CheckDiagonals())
+print(GameBoard.CheckRow())
+print(GameBoard.CheckCol())
